19d3cb2b5d0e973b05d004b39950bd415c16d5f7
Oscar Arrieta <3157893185oscar@gmail.com>
2026-07-20 17:48:21 -0500

    Verion 1.0.4

--- sw.js
+++ sw.js
@@ -1,19 +1,11 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('app-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/index.css'
-      ]);
-    })
-  );
-});
+self.addEventListener("install", () => self.skipWaiting());
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(key => caches.delete(key)))
+        )
+    );
+
+    self.clients.claim();
 });
